35c82eba3a38b45c518083474d7917b7bc52b6f9
youym <36502539+yym0329@users.noreply.github.com>
2022-05-01 15:29:16 +0900

    initial commit

--- srt_reservation.py
+++ srt_reservation.py
@@ -9,21 +9,21 @@ time.sleep(5)
 # print('화면 해상도: ', gw.size())
 
 
-click_times = 8
+click_times = 3
 escape = False
 
 while(1):
   gw.press('f5')
   time.sleep(0.1)
   gw.press('enter')
-  time.sleep(0.2)
+  time.sleep(0.7)
   for i in range(click_times):
-    gw.click(1318, 923, 1)
+    gw.click(1384, 748, 1)
     time.sleep(0.1)
     if keyboard.is_pressed('esc'):
       escape = True
   if escape:
     break
-  time.sleep(0.5)
+  
 
   
